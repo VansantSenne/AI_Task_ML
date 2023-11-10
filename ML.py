c4e50a32922cd9171c3e7cfb2503eead49c92cc1
Senne Vansant <91118511+VansantSenne@users.noreply.github.com>
2023-11-10 14:19:55 +0100

    Update ML.py

--- ML.py
+++ ML.py
@@ -49,7 +49,7 @@ if selected_model == "Gradient Boosting":
     # Confusion Matrix
     cm = confusion_matrix(y_test, gradient_boosting_predictions)
     st.write("Confusion Matrix:")
-    st.write(cm)
+   
     
     # Heatmap
     fig, ax = plt.subplots(figsize=(8, 6))
@@ -58,5 +58,44 @@ if selected_model == "Gradient Boosting":
     ax.set_ylabel('Werkelijke waarden')
     ax.set_title('Confusion Matrix - Gradient Boosting model')
     st.pyplot(fig)
+elif selected_model == "Support Vector Machine (SVM)":
+    # Training van het Support Vector Machine (SVM) model
+    svm_model = SVC(kernel='linear')
+    svm_model.fit(X_resampled, y_resampled)
+    svm_predictions = svm_model.predict(X_test)
+    accuracy = accuracy_score(y_test, svm_predictions) * 100
+    st.write("Support Vector Machine Accuracy:", accuracy, '%')
 
-# Herhaal vergelijkbare stappen voor de andere modellen
+    # Confusion Matrix
+    cm = confusion_matrix(y_test, svm_predictions)
+    st.write("Confusion Matrix:")
+ 
+    
+    # Heatmap
+    fig, ax = plt.subplots(figsize=(8, 6))
+    sns.heatmap(cm, annot=True, fmt='g', cmap='Blues', ax=ax)
+    ax.set_xlabel('Voorspelde waarden')
+    ax.set_ylabel('Werkelijke waarden')
+    ax.set_title('Confusion Matrix - SVM model')
+    st.pyplot(fig)
+
+elif selected_model == "Random Forest":
+    # Training van het Random Forest Classifier model
+    random_forest_model = RandomForestClassifier()
+    random_forest_model.fit(X_resampled, y_resampled)
+    random_forest_predictions = random_forest_model.predict(X_test)
+    accuracy = accuracy_score(y_test, random_forest_predictions) * 100
+    st.write("Random Forest Accuracy:", accuracy, '%')
+
+    # Confusion Matrix
+    cm = confusion_matrix(y_test, random_forest_predictions)
+    st.write("Confusion Matrix:")
+ 
+    
+    # Heatmap
+    fig, ax = plt.subplots(figsize=(8, 6))
+    sns.heatmap(cm, annot=True, fmt='g', cmap='Blues', ax=ax)
+    ax.set_xlabel('Voorspelde waarden')
+    ax.set_ylabel('Werkelijke waarden')
+    ax.set_title('Confusion Matrix - Random Forest Model')
+    st.pyplot(fig)
